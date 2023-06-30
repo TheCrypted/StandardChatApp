71133eedfe68ce2010169fea7274d3c4c5d513bd
Aman Sharma <mail.aman@protonmail.com>
2023-06-30 16:43:44 +0400

    created basic front end elements and added backend setup for socket io server

---
+++ app.cjs
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express();
+const http = require('http');
+const {Server} = require('socket.io')
+const cors = require('cors')
+const {urlencoded} = require("express");
+const PORT = 3030;
+
+app.use(cors());
+app.use(express.json());
+app.use(urlencoded({extended: true}));
+
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors:{
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
+    }
+})
+io.on("connection", (socket)=>{
+    console.log("user connected: " + socket.id)
+    socket.on("sendMessage", (payload)=>{
+        console.log(payload.message)
+    })
+})
+server.listen(PORT, ()=>{
+    console.log("SERVER IS RUNNING ON PORT: " + PORT)
+})
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,32 @@
+
+import io from "socket.io-client"
+
+// const socket = io.connect("http://localhost:3030")
 export const Home = () => {
+	function sendMessage(e){
+		e.preventDefault();
+		socket.emit("sendMessage", {
+			message: "Hello"
+		})
+	}
+
 	return (
 		<>
-			<div className="">Hello</div>
+			<div style={{backgroundImage: "url(https://i.pinimg.com/originals/1a/04/69/1a046940247ca498deffecad4b839fc8.jpg)"}} className="flex items-center justify-center bg-cover w-full h-full">
+				<div className="bg-zinc-800 bg-opacity-60 backdrop-blur-2xl rounded-2xl h-5/6 w-5/6 grid grid-cols-[23%_54%_23%] shadow-2xl">
+					<div className="rounded-l-2xl"></div>
+					<div style={{boxShadow: "inset 0px 0px 100px rgba(0,0,0,0.3)"}} className="bg-zinc-800 bg-opacity-60 grid grid-rows-[90%_10%]">
+						<div className=""></div>
+						<form className="w-full h-full flex">
+							<input type="text" className="focus:outline-none bg-zinc-900 w-5/6 h-full text-white text-2xl font-semibold pl-4" placeholder="Enter message"/>
+							<button type="submit" className="bg-green-500 w-1/6 h-full text-2xl bg-opacity-40 hover:bg-opacity-80 font-semibold" onClick={(e)=> sendMessage(e)}>Send</button>
+						</form>
+					</div>
+					<div className=""></div>
+				</div>
+
+
+			</div>
 		</>
 	)
 }
